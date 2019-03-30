74ae2c88236fb2d050a3f8e09cbe24988bc7d81c
Matheus Protzen <matheus12.kp@gmail.com>
2019-03-30 10:00:47 -0300

    Plots

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-import sys
 import time
 
 from cube import Cube
--- read_file.py
+++ read_file.py
@@ -1,5 +1,5 @@
-def main():
-    file = open(f'teste.txt', 'r')
+def main(n):
+    file = open(f'results/1553942010.1523118/{n}.txt', 'r')
     dados = {}
     algorithm = ''
     for r in file.read().split('\n'):
@@ -10,17 +10,19 @@ def main():
         if 'time' in r:
             time = r.split('time:')[1]
             if time != 'Time out':
-                dados[algorithm]['time'].append(time)
+                dados[algorithm]['time'].append(float(time))
+            else:
+                dados[algorithm]['time'].append(-1)
         if 'nodes' in r:
             nodes = r.split('nodes:')[1]
             if nodes != 'Time out':
-                dados[algorithm]['nodes'].append(nodes)
+                dados[algorithm]['nodes'].append(float(nodes))
+            else:
+                dados[algorithm]['nodes'].append(-1)
         if 'sequence:' in r:
             sequence = r.split('sequence:')[1]
             if sequence != 'Time out':
                 dados[algorithm]['depth'].append(len(str(sequence).split(',')))
+            else:
+                dados[algorithm]['depth'].append(-1)
     return dados
-
-
-if __name__ == '__main__':
-    print(main())
